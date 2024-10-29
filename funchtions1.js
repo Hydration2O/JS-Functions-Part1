0862c88596b2e4f693964eae979b5740cd89189b
Hydration2O <bomalk121@gmail.com>
2024-10-29 20:29:51 +0300

    fixed bug no print

--- funchtions1.js
+++ funchtions1.js
@@ -1,35 +1,40 @@
 function printName() {
-  console.log(`Steve McMain`);
+  console.log("Steve McMain");
 }
+printName();
 function printAge(birthYear) {
   // const birthYear = prompt("write your birth year", 0);
   let currentYear = new Date().getFullYear();
   age = currentYear - birthYear;
-  return age;
+  console.log(`${age}`);
 }
+printAge(1522);
 function printHello(name, language) {
   // const name = prompt("Write your name", "Jane Doe");
   //   const language =prompt("choose language, en = english, es = espaneol, fr = french, tr = arabic", "en");
   switch (language) {
-    case en:
+    case "en":
       console.log(`Hello ${name}`);
       break;
-    case es:
+    case "es":
       console.log(`Hola ${name}`);
       break;
-    case fr:
+    case "fr":
       console.log(`Bonjour${name}`);
       break;
-    case tr:
+    case "tr":
       console.log(`Merhaba ${name}`);
       break;
     default:
       console.log(`Hello ${name}`);
   }
-}
+ }
+printHello("salem", "es");
 function printMax(x, y) {
   // const x= prompt("Enter the first number X", 0);
   // const y=prompt("Enter the second number Y", 1);
   if (x > y) console.log(`X=${x} so it is bigger`);
-  else `Y=${y} so it is bigger`;
+  else console.log( `Y=${y} so it is bigger`);
+
 }
+printMax(5,6);
